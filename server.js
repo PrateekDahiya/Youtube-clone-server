623b0a8a9e57481e6faff46bcd33a8372f4c4f78
Prateek Dahiya <100485095+PrateekDahiya@users.noreply.github.com>
2024-06-09 15:47:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -281,7 +281,7 @@ app.get("/get-subs/:user_id", (req, res) => {
     });
 });
 
-const ytdlpPath = "yt-dlp";
+const ytdlpPath = "./yt-dlp";
 
 app.get("/get-stream-url", (req, res) => {
     const videoUrl = "https://www.youtube.com/watch?v=" + req.query.video_id;
