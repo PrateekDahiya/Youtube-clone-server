faf8dffd31996826cb46b2708fe60f4399ea243b
Prateek Dahiya <100485095+PrateekDahiya@users.noreply.github.com>
2024-06-10 22:45:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const axios = require("axios");
 require("dotenv").config();
 const { exec } = require("child_process");
